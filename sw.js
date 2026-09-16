509d0b4a3f7e1d2d74b30c5250965d1dae37deca
k43144889-web <k43144889@gmail.com>
2026-09-16 22:27:42 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'car-ledger-v1';
+const SHELL = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(SHELL); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  var url = new URL(e.request.url);
+  if(url.origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      var copy = res.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, copy); });
+      return res;
+    }).catch(function(){
+      return caches.match(e.request).then(function(r){ return r || caches.match('./index.html'); });
+    })
+  );
+});
